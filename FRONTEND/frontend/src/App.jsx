ccae6e4ff565c72d7ebd362aaca41b581adeed10
sachindiwakar1802 <sachindiwakar1802@gmail.com>
2026-03-01 01:25:07 +0530

    Update App.jsx

--- FRONTEND/frontend/src/App.jsx
+++ FRONTEND/frontend/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-1-kwan.onrender.com"
+export const ServerUrl  = "https://ai-interview8383.onrender.com"
 
 function App() {
 
